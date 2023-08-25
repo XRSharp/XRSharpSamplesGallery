1c8b66fa54e3bfcfe40ae5f47320bd205405f6de
giovanni-a <8248552+giovanni-a@users.noreply.github.com>
2023-08-25 18:15:46 +0200

    Add Simulator project

---
+++ src/XRSharpSamplesGallery/XRSharpSamplesGallery.Simulator/Startup.cs
@@ -0,0 +1,14 @@
+﻿using OpenSilver.Simulator;
+using System;
+
+namespace XRSharpSamplesGallery.Simulator
+{
+    internal static class Startup
+    {
+        [STAThread]
+        static int Main(string[] args)
+        {
+            return SimulatorLauncher.Start(typeof(App));
+        }
+    }
+}
